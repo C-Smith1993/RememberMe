bd314b9bdd15dd85bd76dd764beab2b6e9989075
C-Smith1993 <chrissmithreds93@hotmail.com>
2017-02-26 09:58:41 +0000

    Added a ! if the task is important.

---
+++ Remember Me/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Remember Me
+//
+//  Created by Chris Smith on 26/02/2017.
+//  Copyright © 2017 CDSApps. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
